d61068e969cc3933d92bb51ea18d807c959bda11
Christian Schudoma <cschu1981@gmail.com>
2021-01-20 18:59:03 +0100

    cleaned output; added in-between 0-count months; github pull requests are ignored

--- issue_grabber.py
+++ issue_grabber.py
@@ -10,7 +10,7 @@ class GitAccess:
 	@classmethod
 	def send_request(cls, owner, repo, token, **params):
 		params[cls.ALL_PARAM[0]] = cls.ALL_PARAM[1]
-		print(params)
+		# print(params)
 		headers = {cls.HEADERS[0]: cls.HEADERS[1].format(token=token)}
 		
 		return requests.get(cls.QUERY_URL.format(owner=owner, repo=repo), 
@@ -45,8 +45,8 @@ def main():
 
 	reply = git.send_request(args.owner, args.repo, args.token, page="0", per_page="1")
 	total_issues = reply[0][git.TICKET_NUMBER] if reply else 0
-	print(reply)
-	print(total_issues)
+	# print(reply)
+	# print(total_issues)
 
 	all_issues = list()
 	old_len = 0
@@ -54,24 +54,40 @@ def main():
 	while len(all_issues) < total_issues:
 		reply = git.send_request(args.owner, args.repo, args.token, page=str(page), per_page=100)
 		all_issues.extend(reply)
-		print(len(all_issues))
+		# print(len(all_issues))
 		if old_len == len(all_issues):
 			break
 		old_len = len(all_issues)
 		page += 1
 
 
-	print(all_issues)
+	# print(all_issues)
 	closed_c, created_c = Counter(), Counter()
 	for item in all_issues:
+		if item.get("pull_request"):
+			continue
 		created_month = item.get("created_at", "")
 		closed_month = item.get("closed_at", "")
 		if closed_month:
 			closed_c[tuple(closed_month.split("-")[:2])] += 1
 		created_c[tuple(created_month.split("-")[:2])] += 1
 
-	for month in sorted(set(created_c).union(closed_c)):
-		print("-".join(month), created_c[month], closed_c[month], sep="\t")
+	last_year, last_month = None, None
+	for year, month in sorted(set(created_c).union(closed_c)):
+		if last_month:
+			current_year, current_month = map(int, (year, month))
+			while True:
+				if last_month == 12:
+					last_year, last_month = last_year + 1, 1
+				else:
+					last_month += 1
+				if (last_year, last_month) == (current_year, current_month):
+					break
+
+				print("{year}-{month:02d}".format(year=last_year, month=last_month), 0, 0, sep="\t")
+
+		print("-".join((year, month)), created_c[(year, month)], closed_c[(year, month)], sep="\t")
+		last_year, last_month = map(int, (year, month))
 	
 
 
